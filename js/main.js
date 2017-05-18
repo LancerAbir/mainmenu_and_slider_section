090f832e0d8ffc434216c774902201efb985fbd5
abir2665266 <dhar.abir.kumar@gmail.com>
2017-05-18 16:35:21 +0600

    mainmenu
    slider_section

---
+++ js/main.js
@@ -0,0 +1,21 @@
+(function($){
+    'use strict';
+
+    $(".homepage-slider").owlCarousel({
+        items : 1,
+        loop : true,
+        nav : false,
+        dots : true,
+        autoplay : false,
+    })
+
+
+
+
+
+
+
+
+
+    
+})(jQuery);
